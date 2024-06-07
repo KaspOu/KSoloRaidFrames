eb067173b5f27c564e874d78ea848d285d0c7bba
Ryad M <ryad@ryme-dev.com>
2024-06-07 05:23:43 +0200

    LibEditMode + UseRaidStylePartyFrames autoactivated
    Thanks to LibEditModeOverride, UseRaidStylePartyFrames autoactivated

--- Core/localization.en.lua
+++ Core/localization.en.lua
@@ -6,7 +6,8 @@ local l = ns.I18N;
 
 l.VERS_TITLE    = format("%s %s", ns.TITLE, ns.VERSION);
 
-l.OPTION_SOLORAID_TOOLTIP = "#"
+l.OPTION_RAIDSTYLE_ACTION = nil
+l.OPTION_RAIDSTYLE_WARNING = nil
 
 l.CONFLICT_MESSAGE = "Disabled: Conflict with %s";
 l.MSG_LOADED         = format("%s loaded and active", l.VERS_TITLE);
@@ -16,5 +17,6 @@ l.INIT_FAILED = format("%s not initialized correctly (conflict?)!", l.VERS_TITLE
 
 -- Edit Mode RaidStyle PF - Since DragonFlight (10)
 if (EditModeManagerFrame.UseRaidStylePartyFrames) then
-  l.OPTION_SOLORAID_TOOLTIP = "I suggest you to activate option "..l.YLL..HUD_EDIT_MODE_SETTING_UNIT_FRAME_RAID_STYLE_PARTY_FRAMES.."|r ("..HUD_EDIT_MODE_MENU..": "..HUD_EDIT_MODE_PARTY_FRAMES_LABEL..")";
+  l.OPTION_RAIDSTYLE_ACTION = "Manually activate option "..l.YLL..HUD_EDIT_MODE_SETTING_UNIT_FRAME_RAID_STYLE_PARTY_FRAMES.."|r ("..HUD_EDIT_MODE_MENU.." : "..HUD_EDIT_MODE_PARTY_FRAMES_LABEL..")";
+  l.OPTION_RAIDSTYLE_WARNING = "Auto-activating option "..l.YLL..HUD_EDIT_MODE_SETTING_UNIT_FRAME_RAID_STYLE_PARTY_FRAMES.."|r ("..HUD_EDIT_MODE_MENU..": "..HUD_EDIT_MODE_PARTY_FRAMES_LABEL..")";
 end
